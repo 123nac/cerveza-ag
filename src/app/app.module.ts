315e06a738de00275e36da9a75634b4a950c554d
fede <fede>
2019-09-20 00:14:41 -0300

    .

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,13 +6,19 @@ import { AppComponent } from './app.component';
 import { NavComponent } from './nav/nav.component';
 import { ListaBirrasComponent } from './lista-birras/lista-birras.component';
 import { ListaCarritoComponent } from './lista-carrito/lista-carrito.component';
+import { GridComponent } from './grid/grid.component';
+import { MainComponent } from './main/main.component';
+import { MainMenuComponent } from './main-menu/main-menu.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     NavComponent,
     ListaBirrasComponent,
-    ListaCarritoComponent
+    ListaCarritoComponent,
+    GridComponent,
+    MainComponent,
+    MainMenuComponent
   ],
   imports: [
     BrowserModule,
