a667418ba867c73a31495e5f5d705fb50139098d
Juan Ignacio Martinez Abadias <jimartinezabadias@gmail.com>
2019-09-19 22:43:39 -0300

    asdasd

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavComponent } from './nav/nav.component';
+import { ListaBirrasComponent } from './lista-birras/lista-birras.component';
+import { ListaCarritoComponent } from './lista-carrito/lista-carrito.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavComponent,
+    ListaBirrasComponent,
+    ListaCarritoComponent
   ],
   imports: [
     BrowserModule,
