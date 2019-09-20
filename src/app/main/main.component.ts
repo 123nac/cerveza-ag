f14c729c948c9c1907e736c0f761c3546c19bfd9
fede <fede>
2019-09-20 02:54:02 -0300

    main

--- src/app/main/main.component.ts
+++ src/app/main/main.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { element } from 'protractor';
+import { Content } from '@angular/compiler/src/render3/r3_ast';
 
 @Component({
   selector: 'app-main',
@@ -10,6 +12,23 @@ export class MainComponent implements OnInit {
   constructor() { }
 
   ngOnInit() {
+    document.querySelector("#js-tab-lista-birras").addEventListener("click", function() {
+      show(document.querySelector("#js-content-lista-birras"));
+    });
+    document.querySelector("#js-tab-lista-carrito").addEventListener("click", function() {
+      show(document.querySelector("#js-content-lista-carrito"));
+    });
+
+    // default
+    show(document.querySelector("#js-content-lista-birras"));
   }
 
+  
+}
+
+function show(content) {
+  document.querySelectorAll(".tab-content").forEach(content => {
+    content.classList.remove("visible");
+  });
+  content.classList.add("visible");
 }
